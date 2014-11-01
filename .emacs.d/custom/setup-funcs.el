fcccb16048856db40e0b8222c237c199bf009e2b
Morten Kristensen <msk@nullpointer.dk>
2014-11-01 02:47:25 +0100

    Added 'C-c w' for enabling/disabling whitespace mode.

--- .emacs.d/custom/setup-funcs.el
+++ .emacs.d/custom/setup-funcs.el
@@ -9,6 +9,7 @@
 (global-set-key (kbd "M-P") 'previous-user-buffer) 
 (global-set-key (kbd "M-N") 'next-user-buffer) 
 (global-set-key (kbd "C-c o") 'ff-find-other-file)
+(global-set-key (kbd "C-c w") 'whitespace-mode)
 
 ;;;;;;;;; FUNCTIONS
 
