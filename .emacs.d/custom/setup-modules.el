6e59400d4eb4537760d3ee09f7fbfa9289fd30fc
Morten Kristensen <msk@nullpointer.dk>
2014-11-01 13:23:40 +0100

    Removed local dtd mode because I don't use it.

--- .emacs.d/custom/setup-modules.el
+++ .emacs.d/custom/setup-modules.el
@@ -69,29 +69,6 @@
                                 ("\\.style$" . css-mode))
                               auto-mode-alist))
 
-;;;;;;;;; DTD mode
-
-(autoload 'dtd-mode "tdtd" "Major mode for SGML and XML DTDs." t)
-(autoload 'dtd-etags "tdtd"
-  "Execute etags on FILESPEC and match on DTD-specific regular expressions."
-  t)
-(autoload 'dtd-grep "tdtd" "Grep for PATTERN in files matching FILESPEC." t)
-
-;; Turn on font lock when in DTD mode
-(add-hook 'dtd-mode-hooks
-          'turn-on-font-lock)
-
-(setq auto-mode-alist
-      (append
-       (list
-        '("\\.dcl$" . dtd-mode)
-        '("\\.dec$" . dtd-mode)
-        '("\\.dtd$" . dtd-mode)
-        '("\\.ele$" . dtd-mode)
-        '("\\.ent$" . dtd-mode)
-        '("\\.mod$" . dtd-mode))
-       auto-mode-alist))
-
 ;;;;;;;;; Csharp-mode
 
 (autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
