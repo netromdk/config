abafd6a188d22201334b5c0f31e648b7f733fb11
Morten Kristensen <msk@nullpointer.dk>
2014-11-01 13:24:28 +0100

    Removed local csharp mode because I don't use it and it's in packages anyway.

--- .emacs.d/custom/setup-modules.el
+++ .emacs.d/custom/setup-modules.el
@@ -69,11 +69,6 @@
                                 ("\\.style$" . css-mode))
                               auto-mode-alist))
 
-;;;;;;;;; Csharp-mode
-
-(autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
-(setq auto-mode-alist (append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
-
 ;;;;;;;;; Haskell-mode
 
 (setq auto-mode-alist
