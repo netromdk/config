f3eaa39dac989c99de361d81165fbeed091680e4
Morten Kristensen <msk@nullpointer.dk>
2015-07-09 15:09:29 +0200

    dash-at-point: c-mode uses 'c,manpages', c++-mode uses 'cpp,qt,c,manpages', and python-mode uses 'py,py2,flask'.

--- .emacs.d/custom/setup-modules.el
+++ .emacs.d/custom/setup-modules.el
@@ -395,9 +395,9 @@
 
 (global-set-key "\C-cd" 'dash-at-point)
 
-(add-to-list 'dash-at-point-mode-alist '(c-mode . "c"))
-(add-to-list 'dash-at-point-mode-alist '(c++-mode . "cpp,qt"))
-(add-to-list 'dash-at-point-mode-alist '(python-mode . "py,py2"))
+(add-to-list 'dash-at-point-mode-alist '(c-mode . "c,manpages"))
+(add-to-list 'dash-at-point-mode-alist '(c++-mode . "cpp,qt,c,manpages"))
+(add-to-list 'dash-at-point-mode-alist '(python-mode . "py,py2,flask"))
 (add-to-list 'dash-at-point-mode-alist '(cmake-mode . "cmake"))
 (add-to-list 'dash-at-point-mode-alist '(js-mode . "js"))
 
