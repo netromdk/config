6cabaa4b1871005136bac1c9da73a328040a0e3a
Morten Kristensen <msk@nullpointer.dk>
2015-07-09 21:26:09 +0200

    dash-at-point: Added 'lux' to c++ mode.

--- .emacs.d/custom/setup-modules.el
+++ .emacs.d/custom/setup-modules.el
@@ -396,7 +396,7 @@
 (global-set-key "\C-cd" 'dash-at-point)
 
 (add-to-list 'dash-at-point-mode-alist '(c-mode . "c,manpages"))
-(add-to-list 'dash-at-point-mode-alist '(c++-mode . "cpp,qt,c,manpages"))
+(add-to-list 'dash-at-point-mode-alist '(c++-mode . "cpp,qt,c,manpages,lux"))
 (add-to-list 'dash-at-point-mode-alist '(python-mode . "py,flask"))
 (add-to-list 'dash-at-point-mode-alist '(cmake-mode . "cmake"))
 (add-to-list 'dash-at-point-mode-alist '(js-mode . "js"))
