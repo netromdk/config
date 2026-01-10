bab1e99b119293d4931745acbc6c44a0c692aa4b
Morten Kristensen <me@mortens.dev>
2026-01-10 17:52:06 +0100

    Deploy scripts folder

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 SELFPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1; pwd -P)"
 CONFIGS="${SELFPATH}/configs"
+SCRIPTS="${SELFPATH}/scripts"
 
-echo "+++ Deploying configs as symlinks +++"
+echo "+++ Deploying configs and scripts as symlinks +++"
 
-for f in $(find "${CONFIGS}" -type f); do
+for f in $(find "${CONFIGS}" -type f) "${SCRIPTS}"; do
   dst="${HOME}/$(basename $f)"
   echo "\n${dst} -> $f"
   if [ -L "${dst}" ]; then
