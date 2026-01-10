b76f7014349bf9ad15f35358a4abb821ab7af922
Morten Kristensen <me@mortens.dev>
2026-01-10 17:55:29 +0100

    Update apps script

--- scripts/update-apps.sh
+++ scripts/update-apps.sh
@@ -2,7 +2,9 @@
 echo "Checking for package updates via APT, Snap, and Flatpak."
 
 echo "\n======= APT ======="
-sudo apt update && sudo apt list --upgradable
+sudo apt update
+sudo apt list --upgradable
+sudo apt upgrade
 
 echo "\n======= Snap ======="
 sudo snap refresh
