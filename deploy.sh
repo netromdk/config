172f60c2581d6e240318165d8821dc7b155630fe
Morten Kristensen <me@mortens.dev>
2026-01-10 16:58:00 +0100

    Config deployment script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+SELFPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1; pwd -P)"
+CONFIGS="${SELFPATH}/configs"
+
+echo "+++ Deploying configs as symlinks +++"
+
+for f in $(find "${CONFIGS}" -type f); do
+  dst="${HOME}/$(basename $f)"
+  echo "\n${dst} -> $f"
+  if [ -L "${dst}" ]; then
+    link="$(readlink ${dst})"
+    if [ "${link}" = "$f" ]; then
+      echo "Already installed."
+      continue
+    fi
+  fi
+  if [ -e "${dst}" ]; then
+    echo "Destination exists. Ignoring."
+    continue
+  fi
+  ln -s "$f" "${dst}" && echo "Installed."
+done
