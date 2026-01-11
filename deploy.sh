c6d18e00a38f4a40acc737ad8d347244f5af8759
Morten Kristensen <me@mortens.dev>
2026-01-11 08:16:21 +0100

    Deploy scripts to PATH

--- deploy.sh
+++ deploy.sh
@@ -2,22 +2,35 @@
 SELFPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1; pwd -P)"
 CONFIGS="${SELFPATH}/configs"
 SCRIPTS="${SELFPATH}/scripts"
+PATHFLD="${HOME}/.local/bin"
+
+deploy() {
+  SRC="$1"
+  DSTFLD="$2"
+  DST="${DSTFLD}/$(basename ${SRC})"
+  echo "\n${DST} -> ${SRC}"
+  if [ -L "${DST}" ]; then
+    if [ "$(readlink ${DST})" = "${SRC}" ]; then
+      echo "Already installed."
+      return
+    fi
+  fi
+  if [ -e "${DST}" ]; then
+    echo "Destination exists. Ignoring."
+    return
+  fi
+  ln -s "${SRC}" "${DST}" && echo "Installed."
+}
 
 echo "+++ Deploying configs and scripts as symlinks +++"
 
 for f in $(find "${CONFIGS}" -type f) "${SCRIPTS}"; do
-  dst="${HOME}/$(basename $f)"
-  echo "\n${dst} -> $f"
-  if [ -L "${dst}" ]; then
-    link="$(readlink ${dst})"
-    if [ "${link}" = "$f" ]; then
-      echo "Already installed."
-      continue
-    fi
-  fi
-  if [ -e "${dst}" ]; then
-    echo "Destination exists. Ignoring."
-    continue
-  fi
-  ln -s "$f" "${dst}" && echo "Installed."
+  deploy "$f" "${HOME}"
+done
+
+echo "\n+++ Deploying scripts to PATH  +++"
+
+mkdir -p "${PATHFLD}"
+for f in $(find "${SCRIPTS}" -type f); do
+  deploy "$f" "${PATHFLD}"
 done
