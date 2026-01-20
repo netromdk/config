4e62fff8d7cb5783d7829f4c6893e78a2fa7f669
Morten Kristensen <me@mortens.dev>
2026-01-20 09:44:11 +0100

    [update-apps] Support Homebrew on macOS

--- scripts/update-apps.sh
+++ scripts/update-apps.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-echo "Checking for package updates via APT, Snap, and Flatpak."
 
 check_program() {
   if hash $1 2> /dev/null; then
@@ -9,37 +8,52 @@ check_program() {
   return 1
 }
 
-echo "\n======= APT ======="
-check_program apt
-if [ $? -eq 0 ]; then
-  set -x
-  sudo apt update
-  apt list --upgradable
-  sudo apt upgrade
-  set +x
-fi
+DIST=$(uname -s)
 
-echo "\n======= Snap ======="
-check_program snap
-if [ $? -eq 0 ]; then
-  CMD="sudo snap refresh --color=auto"
-  set -x
-  # "All snaps up to date." is written to stderr and we want it ignored and only written with the
-  # last snap invocation.
-  ${CMD} --list 2>/dev/null
-  set +x
-  NUMPKGS=$(${CMD} --list 2>/dev/null | wc -l)
-  if [ $NUMPKGS -gt 0 ]; then
-    echo "Note: Update running apps by rerunning $0 after terminating them."
+if [ "${DIST}" = "Linux" ]; then
+  echo "Checking for package updates via APT, Snap, and Flatpak."
+  echo "\n======= APT ======="
+  check_program apt
+  if [ $? -eq 0 ]; then
+    set -x
+    sudo apt update
+    apt list --upgradable
+    sudo apt upgrade
+    set +x
   fi
-  set -x
-  ${CMD}
-  set +x
-fi
 
-echo "\n======= Flatpak ======="
-check_program flatpak
-if [ $? -eq 0 ]; then
-  set -x
-  flatpak update
+  echo "\n======= Snap ======="
+  check_program snap
+  if [ $? -eq 0 ]; then
+    CMD="sudo snap refresh --color=auto"
+    set -x
+    # "All snaps up to date." is written to stderr and we want it ignored and only written with the
+    # last snap invocation.
+    ${CMD} --list 2>/dev/null
+    set +x
+    NUMPKGS=$(${CMD} --list 2>/dev/null | wc -l)
+    if [ $NUMPKGS -gt 0 ]; then
+      echo "Note: Update running apps by rerunning $0 after terminating them."
+    fi
+    set -x
+    ${CMD}
+    set +x
+  fi
+
+  echo "\n======= Flatpak ======="
+  check_program flatpak
+  if [ $? -eq 0 ]; then
+    set -x
+    flatpak update
+  fi
+
+elif [ "${DIST}" = "Darwin" ]; then
+  echo "Checking for package updates via Homebrew."
+  check_program brew
+  if [ $? -eq 0 ]; then
+    set -x
+    brew update
+    brew outdated
+    brew upgrade
+  fi
 fi
